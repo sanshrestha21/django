a0707b6834d8e38389502903b1febf471f5834e9
San Shrestha <sanshrestha21@gmail.com>
2023-10-06 12:54:39 +0545

    Record Updated

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .templates.forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm, UpdateRecordForm
 from .models import Record
 
 # Create your views here.
@@ -72,3 +72,31 @@ def delete_record(request, pk):
     else:
          messages.success(request, "You Must Be Logged In To Delete Record!")
          return redirect ('home') 
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added...")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+
+        current_record = Record.objects.get(id=pk)
+        form = UpdateRecordForm(request.POST or None, instance=current_record)
+        if request.method == "POST":
+            if form.is_valid():
+                update_record = form.save()
+                messages.success(request, "Record Updated...")
+                return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
